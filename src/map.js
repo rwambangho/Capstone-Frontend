a99b2167d433c3fc0c8a88dc99b7e81bcaf328ca
kimseungzzang <99kimst@gmail.com>
2024-03-12 00:09:22 +0900

    카카오 맵 api 연결

---
+++ src/map.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+const MapComponent = () => {
+    useEffect(() => {
+        // Kakao Maps 초기화 및 사용하는 코드 작성..
+        const container = document.getElementById('map');
+        const options = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3
+        };
+        const map = new window.kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <div id="map" style={{ width: '500px', height: '400px' }}></div>
+    );
+};
+
+export default MapComponent;
