65a2c71ef334abde890fb6eaa3ab1e8c659f1ff9
Kim Haeun <128309139+KimHaeun123@users.noreply.github.com>
2024-04-08 23:55:00 +0900

    chatlist초본

--- src/mypage/ChatList.js
+++ src/mypage/ChatList.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import '../css/MyPage.css';
-import Navbar from '../component/Navbar';
 
 function ChatList() {
+  const [chats, setChats] = useState([]);
+  const userId1 = '';
+  const userId2 = '';
+
+  useEffect(() => {
+    const fetchChatList = async () => {
+      try {
+        const response = await axios.get(`/chatlist/${userId1}/${userId2}`);
+
+        setChats(response.data);
+      } catch (error) {
+        console.error('Error fetching chat list:', error);
+      }
+    };
+
+    fetchChatList();
+  }, [userId1, userId2]);
+
   return (
     <div>
-      <Navbar />
       <h1>Chat List</h1>
+      <ul>
+        {chats.map((chat) => (
+          <li key={chat.chatRoomNumber}>
+            <Link to={`/chat?userId1=${chat.userId1}&userId2=${chat.userId2}`}>
+              {`Chat with ${chat.userId2}`}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
 
 export default ChatList;
